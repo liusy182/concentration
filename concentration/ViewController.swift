1de1708ba9d157e76c3f35028cd5a8b9ee15c0c8
liusy182 <liusy182@gmail.com>
2016-03-25 15:57:36 +0800

    changed color

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -12,26 +12,55 @@ enum Difficulty {
     case Easy, Medium, Hard
 }
 
+extension UIColor {
+    //type method (a.k.a statis method)
+    class func greenSea() -> UIColor {
+        return .colorComponents((22, 160, 133))
+    }
+    
+    class func emerald() -> UIColor {
+        return  .colorComponents((46, 204, 113))
+    }
+    
+    class func sunflower() -> UIColor {
+        return .colorComponents((241, 196, 15))
+    }
+    
+    class func alizarin() -> UIColor {
+        return  .colorComponents((231, 76, 60))
+    }
+    
+    class func colorComponents(
+        components: (CGFloat, CGFloat, CGFloat)) -> UIColor {
+        
+        return UIColor(
+            red: components.0/255,
+            green: components.1/255,
+            blue: components.2/255,
+            alpha: 1)
+    }
+}
+
 private extension ViewController {
     
     func setup() {
-        view.backgroundColor = .whiteColor()
+        view.backgroundColor = .greenSea()
         buildButtonWithCenter(
             CGPoint(x: view.center.x, y: view.center.y/2.0),
             title: "EASY",
-            color: .greenColor(),
+            color: .emerald(),
             action: #selector(ViewController.onEasyTapped(_:)))
         
         buildButtonWithCenter(
             CGPoint(x: view.center.x, y: view.center.y),
             title: "MEDIUM",
-            color: .yellowColor(),
+            color: .sunflower(),
             action: #selector(ViewController.onMediumTapped(_:)))
         
         buildButtonWithCenter(
             CGPoint(x: view.center.x, y: view.center.y*3.0/2.0),
             title: "HARD",
-            color: .redColor(),
+            color: .alizarin(),
             action: #selector(ViewController.onHardTapped(_:)))
         
     }
@@ -44,7 +73,7 @@ private extension ViewController {
         
         let button = UIButton()
         button.setTitle(title, forState: .Normal)
-        button.setTitleColor(.blackColor(), forState: .Normal)
+        button.setTitleColor(.whiteColor(), forState: .Normal)
         button.frame = CGRect(
             origin: CGPoint(x: 0, y: 0),
             size: CGSize(width: 200, height: 50))
